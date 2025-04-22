106132c26a7d5f8beccdf70c0b65009de2e19461
yumachin <curryuma925@gmail.com>
2025-04-22 22:01:41 +0900

    ボタンのスタイルを強調し、削除および編集モーダルのボタンにフォントボールドを追加しました。

--- src/components/modals/EditInterview.tsx
+++ src/components/modals/EditInterview.tsx
@@ -113,7 +113,7 @@ export default function EditInterview(props: EditInterviewProps) {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <button className="px-2 py-0.5 text-sm rounded-sm shadow-md bg-emerald-600 text-white">
+        <button className="px-2 py-0.5 text-sm rounded-sm shadow-md bg-emerald-600">
           編集
         </button>
       </DialogTrigger>
