feebdd18ff4fa9f06a6efd8fefda5b82146d4b27
yumachin <curryuma925@gmail.com>
2025-06-13 18:12:15 +0900

    ミドルウェアのコードをコメントアウトしました。

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,23 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
 
-export function middleware(req: NextRequest) {
-  const accessToken = req.cookies.get('access-token')?.value
-  console.log('Access Token:', accessToken)
-  const { pathname } = req.nextUrl
+// export function middleware(req: NextRequest) {
+//   const accessToken = req.cookies.get('access-token')?.value
+//   console.log('Access Token:', accessToken)
+//   const { pathname } = req.nextUrl
 
-  if (
-    (pathname.startsWith('/company') || pathname.startsWith('/dashboard') || pathname.startsWith('/interview')
-    || pathname.startsWith('/meeting') || pathname.startsWith('/submission')) && !accessToken
-  ) {
-    const loginUrl = req.nextUrl.clone()
-    loginUrl.pathname = '/'
-    return NextResponse.redirect(loginUrl)
-  }
+//   if (
+//     (pathname.startsWith('/company') || pathname.startsWith('/dashboard') || pathname.startsWith('/interview')
+//     || pathname.startsWith('/meeting') || pathname.startsWith('/submission')) && !accessToken
+//   ) {
+//     const loginUrl = req.nextUrl.clone()
+//     loginUrl.pathname = '/'
+//     return NextResponse.redirect(loginUrl)
+//   }
 
-  return NextResponse.next()
-}
+//   return NextResponse.next()
+// }
 
-export const config = {
-  matcher: ['/company/:path*', '/dashboard/:path*', '/interview/:path*', '/meeting/:path*', '/submission/:path*']
-}
+// export const config = {
+//   matcher: ['/company/:path*', '/dashboard/:path*', '/interview/:path*', '/meeting/:path*', '/submission/:path*']
+// }
